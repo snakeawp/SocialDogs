03962010912692f54679e6a161bf00175fd0f82c
CLAUDIO RODRIGUES <90005067@pmenos.com.br>
2021-08-03 15:00:22 -0300

    alterações pag login

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -6,7 +6,7 @@ import { UserContext } from '../../UserContext'
 
 const Header = () => {
 
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -15,7 +15,7 @@ const Header = () => {
           <Dogs />
         </Link>
         {data ? (<Link className={styles.login} to="/conta">{data.nome}
-          <button onClick={userLogout}>Sair</button>
+          {/* <button onClick={userLogout}>Sair</button> */}
         </Link>)
           : (<Link className={styles.login} to="/login">Login / Criar</Link>)}
 
